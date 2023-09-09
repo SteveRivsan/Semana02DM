13ed1f90c41adaca08de2064d71744491814a67a
laboratorioesan <laboratorioesan@esan.edu.pe>
2023-09-09 09:38:53 -0500

    Test

---
+++ app/src/main/java/com/example/semana02/SecondaryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.semana02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secondary)
+        //Code
+    }
+}
